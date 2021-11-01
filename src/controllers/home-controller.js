9b34de25c067c8b47ac95a987e2cad63c3f9b686
toshko89 <todor_petkov@mail.bg>
2021-11-01 11:36:06 +0200

    profile page updated

--- src/controllers/home-controller.js
+++ src/controllers/home-controller.js
@@ -2,16 +2,13 @@ const homeController = require('express').Router();
 const { authorization } = require('../middleWares/auth-middleware.js');
 const postService = require('../services/post-service.js');
 
-
-
 homeController.get('/', (req, res) => {
     res.render('home')
 });
 
 homeController.get('/profile', authorization, async (req, res) => {
     const post = await postService.getAllPerUser(req.user._id);
-    const userName = req.user.userName;
-    res.render('my-posts', { post,userName });
+    res.render('my-posts', { post });
 });
 
 module.exports = homeController;
